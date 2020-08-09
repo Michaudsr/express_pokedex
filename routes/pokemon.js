c60dd8896231864d0837688868fdef7a7171d0ca
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-08-09 10:55:55 -0400

    [pokedex] adding css background and button

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -26,7 +26,7 @@ const axios = require('axios');
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  try {
+  do {
     await db.pokemon.findOrCreate({
       where: {
         name: req.body.name
@@ -34,7 +34,7 @@ router.post('/', async (req, res) => {
     })
     res.redirect('/pokemon')
 
-  } catch (err){
+  } while (err){
     console.log('Error')
   }
 });
@@ -42,29 +42,29 @@ router.post('/', async (req, res) => {
 
 
 router.get('/:name', async (req,res) => {
-  try {
+  do {
     if (req.params && req.params.name){
       const pokemonURL = `https://pokeapi.co/api/v2/pokemon/${req.params.name.toLowerCase()}`;
       const result = await axios.get(pokemonURL);
       let pokemonResults = result.data;
       res.render("show", {pokedata: pokemonResults})
     }
-  } catch (err) {
+  } while (err) {
     res.send("error");
   }
 })
 
-router.get('/:name', async (req,res) =>{
-  try {
-    await db.pokemon.destroy({
-      where: {
-        name: req.body.name
-      },
-    })
-    res.redirect('/')
-  } catch (err){
-    console.log('Error')
-  }
-})
+// router.get('/:name', async (req,res) =>{
+//   do {
+//     await db.pokemon.destroy({
+//       where: {
+//         name: req.body.name
+//       },
+//     })
+//     res.redirect('/')
+//   } while (err){
+//     console.log('Error')
+//   }
+// })
 
 module.exports = router;
