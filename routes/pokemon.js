9c83d5bb0ab6c0db4e53db4ac18645bd263d492f
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-08-09 12:31:41 -0400

    [pokedex] delete button

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,15 +6,16 @@ const router = express.Router();
 const db = require('../models');
 const { response } = require('express');
 const axios = require('axios');
+const methodOverride = require('method-override');
 
 
 // GET /pokemon - return a page with favorited Pokemon
   router.get('/', async (req, res) => {
-    try {
-      const pokeFind = await db.pokemon.findAll(); //grab info from pokemon data table
+    try{
+      const pokeFind = await db.pokemon.findAll(); 
       res.render("favorites", {pokemon: pokeFind})
     } catch (err){
-      res.send("Error pokemon deosnt exist");
+      res.send(error, "Error");
     }
   });
  
@@ -26,7 +27,7 @@ const axios = require('axios');
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  do {
+  try {
     await db.pokemon.findOrCreate({
       where: {
         name: req.body.name
@@ -34,37 +35,39 @@ router.post('/', async (req, res) => {
     })
     res.redirect('/pokemon')
 
-  } while (err){
-    console.log('Error')
+  } catch (err){
+    console.log('Error', error)
   }
 });
 
 
 
 router.get('/:name', async (req,res) => {
-  do {
+ try {
     if (req.params && req.params.name){
       const pokemonURL = `https://pokeapi.co/api/v2/pokemon/${req.params.name.toLowerCase()}`;
       const result = await axios.get(pokemonURL);
-      let pokemonResults = result.data;
-      res.render("show", {pokedata: pokemonResults})
+      let pokemonDetails = result.data;
+      res.render("show", {pokedata: pokemonDetails})
     }
-  } while (err) {
+  } catch(err) {
     res.send("error");
   }
 })
 
-// router.get('/:name', async (req,res) =>{
-//   do {
-//     await db.pokemon.destroy({
-//       where: {
-//         name: req.body.name
-//       },
-//     })
-//     res.redirect('/')
-//   } while (err){
-//     console.log('Error')
-//   }
-// })
+router.delete('/', async (req, res) => {
+  try {
+    await db.pokemon.destroy({
+      where: {
+        name: req.body.name
+      },
+    })
+    res.redirect('/pokemon')
+
+  } catch(err){
+    console.log('Error')
+  }
+});
+
 
 module.exports = router;
